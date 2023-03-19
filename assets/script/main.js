0d3838ca25306df18c399e142cb9650c4eedc138
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2023-03-19 15:16:13 +0100

    animation h1 titre à propos

--- assets/script/main.js
+++ assets/script/main.js
@@ -447,6 +447,31 @@ imageParallax.fromTo(
   }
 );
 
+//animate About title
+const aboutTitle = gsap.timeline({
+  ease: Power1.easeOut,
+  scrollTrigger: {
+    trigger: ".bout-header",
+    start: "top",
+    scrub: true,
+    end: "bottom 0%",
+  },
+});
+
+aboutTitle.fromTo(
+  ".headline.about",
+  {
+    willChange: "transform",
+    y: "0%",
+    transformStyle: "preserve-3d",
+  },
+  {
+    willChange: "transform",
+    y: "160%",
+    transformStyle: "preserve-3d",
+  }
+);
+
 // Gallery animée
 let medium = gsap.matchMedia();
 
@@ -560,49 +585,3 @@ Object.keys(breakpoints).forEach((breakpoint) => {
   // Définir la largeur initiale de l'élément
   setElementStyle();
 });
-
-//slider infini fonction draggable et snap
-
-// Select slider elements
-const slider = document.querySelector("#slider-track");
-const sliderInner = document.querySelector("#slider-list");
-const cards = document.querySelectorAll(".slide-card");
-
-// Initialize GSAP Draggable plugin
-gsap.registerPlugin(Draggable);
-
-// Set initial position of slider
-let position = -cards[0].offsetLeft;
-gsap.set(sliderInner, { x: position });
-
-// Create Draggable instance for slider
-const draggable = Draggable.create(sliderInner, {
-  type: "x",
-  edgeResistance: 0.5,
-  bounds: {
-    minX: -cards[cards.length - 1].offsetLeft,
-    maxX: 0,
-  },
-  onDragEnd: updatePosition,
-});
-
-// Update position of slider on drag end
-function updatePosition() {
-  // Get current position of slider
-  const currentPosition = -sliderInner.getBoundingClientRect().left;
-
-  // Loop through each card and check if it's in the center
-  cards.forEach((card) => {
-    const cardLeft = card.getBoundingClientRect().left;
-    const cardRight = cardLeft + card.offsetWidth;
-
-    // If card is in center, animate to center position
-    if (cardLeft < currentPosition && cardRight > currentPosition) {
-      gsap.to(sliderInner, {
-        duration: 0.3,
-        x: -card.offsetLeft,
-        ease: "power3.inOut",
-      });
-    }
-  });
-}
